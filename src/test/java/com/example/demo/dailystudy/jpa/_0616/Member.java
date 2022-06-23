e2a9d1c8b9fbef9b24d827995db987b5e11b4bb3
heeyeon22 <80864253+heeyeon22@users.noreply.github.com>
2022-06-23 14:53:36 +0900

    회원클래스 생성

---
+++ src/test/java/com/example/demo/dailystudy/jpa/_0616/Member.java
@@ -0,0 +1,30 @@
+package com.example.demo.dailystudy.jpa._0616;
+
+public class Member {
+    private String id;
+    private String username;
+    private Integer age;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username=username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
